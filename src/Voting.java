745695ba6048385a14b8a016bdf5abbba8c3dc69
akshata <akshatamanturi97@gmail.com>
2024-08-13 18:48:35 +0530

    commit

---
+++ src/Voting.java
@@ -0,0 +1,15 @@
+public class Voting {
+
+    public static void main(String[] args) {
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("eligible for voting");
+        } else {
+            System.out.println("not eligible");
+        }
+
+
+    }
+}
+
+
