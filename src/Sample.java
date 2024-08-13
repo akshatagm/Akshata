45358ac143c56736978109aedb6ad0634ae68bcf
akshata <akshatamanturi97@gmail.com>
2024-08-13 23:09:55 +0530

    second pgm

---
+++ src/Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+
+    public static void main(String[] args) {
+        int i=5;
+        if(i%2==0)
+        {
+            System.out.println("even no");
+        }
+        else {
+            System.out.println("odd no");
+        }
+    }
+
+}
